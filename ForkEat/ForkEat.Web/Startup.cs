32bb116c6922065e11ccadedaf2fa2e69b57c885
Arsène Lapostolet <arsene@lapostolet.fr>
2021-09-14 15:50:55 +0200

    start to setup db context

--- ForkEat/ForkEat.Web/Startup.cs
+++ ForkEat/ForkEat.Web/Startup.cs
@@ -2,15 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ForkEat.Web.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Npgsql;
 
 namespace ForkEat.Web
 {
@@ -26,6 +29,9 @@ namespace ForkEat.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(GetPostgresConnectionString()));
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -50,6 +56,29 @@ namespace ForkEat.Web
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+        
+        private string GetPostgresConnectionString()
+        {
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            if (databaseUrl is null)
+            {
+                throw new ArgumentException("Please populate the DATABASE_URL env variable");
+            }
+
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/')
+            };
+
+            return builder.ToString();
         }
     }
 }
