e086974f700aaed1f37bf08c8446396d49681b8b
Arsène Lapostolet <arsene@lapostolet.fr>
2021-09-15 08:40:12 +0200

    starting to implement auth

---
+++ ForkEat/ForkEat.Core/Contracts/RegisterUserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ForkEat.Core.Contracts
+{
+    public class RegisterUserRequest
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ForkEat/ForkEat.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using ForkEat.Core.Contracts;
+using ForkEat.Core.Domain;
+
+namespace ForkEat.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<User> Login(string email, string password);
+        Task<User> Register(RegisterUserRequest request);
+    }
+}
