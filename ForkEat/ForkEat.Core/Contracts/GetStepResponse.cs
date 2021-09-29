4cb64cd2cd54a319bff47626544c7028b31831ea
Ombrelin <arsene@lapostolet.fr>
2021-09-29 10:46:20 +0200

    start to implement

--- ForkEat/ForkEat.Core/Contracts/GetStepResponse.cs
+++ ForkEat/ForkEat.Core/Contracts/GetStepResponse.cs
@@ -9,13 +9,11 @@ namespace ForkEat.Core.Contracts
         
         public GetStepResponse(Step step)
         {
-            Id = step.Id;
             Name = step.Name;
             EstimatedTime = step.EstimatedTime;
             Instructions = step.Instructions;
         }
 
-        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Instructions { get; set; }
         public TimeSpan EstimatedTime { get; set; }
