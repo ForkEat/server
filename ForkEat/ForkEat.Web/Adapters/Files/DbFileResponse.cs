2b772c2f6fbbbf8a200d755bf408b1a30d3d418c
Arsène Lapostolet <arsene@lapostolet.fr>
2021-10-16 12:35:26 +0200

    create db file returns file infos

---
+++ ForkEat/ForkEat.Web/Adapters/Files/DbFileResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ForkEat.Web.Adapters.Files
+{
+    public class DbFileResponse
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+    }
+}
