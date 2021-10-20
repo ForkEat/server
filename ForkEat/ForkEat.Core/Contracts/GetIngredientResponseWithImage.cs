899250652d47b9800c5b7adb2c47e45cde91a804
Marion Armengaud <armengaudmarion@gmail.com>
2021-10-20 10:35:54 +0200

    adding image id and unit id on get one recipe

---
+++ ForkEat/ForkEat.Core/Contracts/GetIngredientResponseWithImage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ForkEat.Core.Domain;
+
+namespace ForkEat.Core.Contracts
+{
+    public class GetIngredientResponseWithImage
+    {
+        public GetIngredientResponseWithImage()
+        {
+        }
+
+        public GetIngredientResponseWithImage(Ingredient ingredient)
+        {
+            ImageId = ingredient.Product.ImageId;
+            Name = ingredient.Product.Name;
+            ProductId = ingredient.Product.Id;
+            Quantity = ingredient.Quantity;
+            Unit = new UnitResponse(ingredient.Unit);
+        }
+
+        public Guid ImageId { get; set; }
+        public string Name { get; set; }
+        public Guid ProductId { get; set; }
+        public uint Quantity { get; set; }
+        public UnitResponse Unit { get; set; }
+    }
+}
