14fc9fcdcfddb0e477beef82c15b489d12b10233
Arsène Lapostolet <arsene@lapostolet.fr>
2021-11-08 13:33:22 +0100

    update quantity to be double in db

---
+++ ForkEat/ForkEat.Web/Migrations/20211108123205_quantityDouble.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ForkEat.Web.Migrations
+{
+    public partial class quantityDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredients_Products_ProductId",
+                table: "Ingredients");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Quantity",
+                table: "Ingredients",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "Ingredients",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredients_Products_ProductId",
+                table: "Ingredients",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredients_Products_ProductId",
+                table: "Ingredients");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Quantity",
+                table: "Ingredients",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "Ingredients",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredients_Products_ProductId",
+                table: "Ingredients",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
